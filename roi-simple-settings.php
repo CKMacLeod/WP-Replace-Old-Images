c9bc2516993227d1f89bbae2e6f9f391b40a598a
CKMacLeod <ckm@ckmacleod.com>
2015-09-15 10:20:37 -0700

    Readability and Settings Page Edits

--- roi-simple-settings.php
+++ roi-simple-settings.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Replace Old and Broken Images/Links
  * Plugin URI: http://ckmacleod.com/roi-wp-plugin/
- * Description: A WordPress plug-in for replacing old, broken, and unwanted images with a user-adjustable default image, according to date or file location
+ * Description: A WordPress plug-in for replacing old, broken, and unwanted images with a default image, according to date or file location.
  * Version: 1.0
  * Author: CK MacLeod
  * Date: September 14, 2015
@@ -19,7 +19,10 @@
  * @Second String Match: Any secondary part (by default the typical WordPress image folder name "uploads"
  * @Version: Beginning 1.0
  * 
- */     
+ */   
+
+defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
+
 register_activation_hook( __FILE__,'roi_set_default_options');
 
 function roi_set_default_options() {
@@ -60,43 +63,74 @@ function roi_register_settings() {
 }
 
 function roi_settings_page() {
-?>
-	<div class="wrap">
-	<h2>Replace Old Images</h2>
+    
+    ?>
+    
+    <div class="wrap">
 
-	<form method="post" action="options.php">
+        <h2>Replace Old Images</h2>
+
+        <p >All displayed images (&lt;img src=...) and image anchor links (&lt;a href=...) with extensions ".gif," ".jpg," ".jpeg," or ".png", in (1) posts up to or on the Cut-Off Date, which also, (2a) and (2b) contain the specified strings of characters, will be replaced in post content by (3) the image or link supplied by URL and shown below.</p> <p>If dimensions are hard-coded in the original image display code, the replacement image will take on those dimensions.</p><p>Remember to Save Changes!</p>
+
+        <form method="post" action="options.php">
+
 		<?php settings_fields( 'roi-settings-group' ); ?>
+            
 		<?php $roi_options = get_option( 'roi_options' ); ?>
+            
 		<table class="form-table">
             
 <!--            check functions if any of "name"-->
             <tr valign="top">
-                <th scope="row">Cut-Off Date  </th>
-                <td><input type="text" name="roi_options[roi_cut_off_date]" value="<?php echo esc_attr($roi_options['roi_cut_off_date']); ?>" /><br><label>Any readable full date format... </label></td>
+                <th scope="row">1. Cut-Off Date  </th>
+            
+                <td><input type="text" name="roi_options[roi_cut_off_date]" value="<?php echo esc_attr($roi_options['roi_cut_off_date']); ?>" /><p class="description">Any readable full date format... </p></td>
+            
             </tr>
+            
             <tr valign="top">
-                <th scope="row">Image URL   </th>
-                <td><input type="text" size="75" name="roi_options[roi_image_url]" value="<?php echo esc_attr($roi_options['roi_image_url']); ?>" /><br><label>Use full URL (http://... etc.) </label><p>Current Image: </p>
-                <img src="<?php echo esc_attr($roi_options['roi_image_url']); ?>"></td>
+            
+                <th scope="row">2a. First String Match</th>
+
+                <td><input type="text" name="roi_options[roi_match_1]" value="<?php echo esc_attr($roi_options['roi_match_1']); ?>" /><p class="description">Default: Host Name</p></td>
+
             </tr>
+
             <tr valign="top">
-                <th scope="row">First String Match (Host Name)  </th>
-                <td><input type="text" name="roi_options[roi_match_1]" value="<?php echo esc_attr($roi_options['roi_match_1']); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"> Second String Match (Directory)  </th>
-                <td><input type="text" name="roi_options[roi_match_2]" value="<?php echo esc_attr($roi_options['roi_match_2']); ?>" /></td>
+
+                <th scope="row">2b. Second String Match</th>
+
+                <td><input type="text" name="roi_options[roi_match_2]" value="<?php echo esc_attr($roi_options['roi_match_2']); ?>" /><p class="description">Default: Uploads Directory</p></td>
+
             </tr>        
+
+            <tr valign="top">
+
+                <th scope="row">3. URL to "Image Removed" Image</th>
+
+                <td><input type="text" size="75" name="roi_options[roi_image_url]" value="<?php echo esc_attr($roi_options['roi_image_url']); ?>" />
+                    
+                    <p class="description">Use full URL (http://... etc.) </p>
+                    
+                    <h3>Currently In Use: </h3>
+
+                    <img src="<?php echo esc_attr($roi_options['roi_image_url']); ?>"></td>
+            
+            </tr>
+            
         </table>
 
 		<p class="submit">
-			<input type="submit" class="button-primary"	value="Save Changes" />
-		</p>
+	
+                    <input type="submit" class="button-primary" value="Save Changes" />
+		
+                </p>
 
 	</form>
-	</div>
+	
+    </div> <!--Settings Page HTML "wrap" -->
 
-<?php
+    <?php
 
 }
 
